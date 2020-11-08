dfb4944dc611daaf0cd2a130cddfa3b50ffcb300
Amy <aljustice25@gmail.com>
2020-11-09 11:05:14 +1300

    initial commit

---
+++ src/components/FormTerms.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton'
+
+export class FormTerms extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
+    render() {
+        const { values } = this.props;
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title = "Get a quote" />
+                    <h2>Privacy Policy, Terms and Conditions</h2>
+                    <p>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+                    </p>
+
+                    <RaisedButton 
+                        label = "Continue"
+                        primary = {true}
+                        style= {styles.button}
+                        onClick = { this.continue() } >
+                    </RaisedButton>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default FormTerms
---
+++ src/components/UserForm.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import FormTerms from './FormTerms'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        terms: false,
+        carRego: '',
+        license: {
+            firstName: '',
+            lastName: '',
+            licenseNumber: ''
+        },
+        otherDrivers: false,
+        address: '',
+        parkLocation: '',
+        carKms: '',
+        carHistory: '',
+        existingCustomer: false
+    }
+
+    // proceed to next step
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+    // go back to previous step
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        });
+    }
+
+    // handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
+    render() {
+        const { step } = this.state;
+        const { terms, carRego, license, otherDrivers, address, parkLocation, carKms, carHistory, existingCustomer } = this.state;
+
+        const values = { terms, carRego, license, otherDrivers, address, parkLocation, carKms, carHistory, existingCustomer }
+
+        switch(step) {
+            case 1:
+                return (
+                    <FormTerms 
+                        nextStep = {this.nextStep}
+                        handleChange = {this.handleChange}
+                        values = {values}
+                    />
+                )
+            case 2:
+                return (
+                    <h1>FormCarRego</h1>
+                )
+            case 3:
+                return (
+                    <h1>FormLicense</h1>
+                )
+            case 4:
+                return (
+                    <h1>FormOtherDrivers</h1>
+                )
+            case 5:
+                return (
+                    <h1>FormDriverUse</h1>
+                )
+            case 6:
+                return (
+                    <h1>FormAddress</h1>
+                ) 
+            case 7:
+                return (
+                    <h1>FormPark</h1>
+                ) 
+            case 8:
+                return (
+                    <h1>FormKms</h1>
+                ) 
+            case 9:
+                return (
+                    <h1>FormHistory</h1>
+                ) 
+            case 10:
+                return (
+                    <h1>FormExistingCustomer</h1>
+                ) 
+            case 11:
+                return (
+                    <h1>FormQuote</h1>
+                ) 
+            default:
+                <h1>Default</h1>
+        }
+    }
+}
+
+export default UserForm
